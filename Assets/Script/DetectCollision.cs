a7229361a4f47fc984d4fab4ea898e8aa0a34ed0
Ly_Man <lym861@hsd.k12.or.us>
2025-05-14 12:24:08 -0700

    fixing the Score can add score when collide with obstacle

--- Assets/Script/DetectCollision.cs
+++ Assets/Script/DetectCollision.cs
@@ -2,28 +2,22 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    private ScoreManager scoreManager;
-    private bool getScore = false;
+    public ScoreManager scoreManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        scoreManager = GetComponent<ScoreManager>();
+        scoreManager = FindObjectOfType<ScoreManager>();
     }
     // Update is called once per frame
     void Update()
     {
-    if (getScore)
-            {
-                // Update score
-                scoreManager.UpdateScore(5);
-                getScore = false;
-            }
+        
     }
     public void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
         Destroy(gameObject);
-        getScore = true;
+        scoreManager.UpdateScore(10);
     }
     
 }
