83f463ab3872c0f3b11a25289357d3bbfe11cd7d
Ly_Man <lym861@hsd.k12.or.us>
2025-05-19 12:17:17 -0700

    adding script that if there are so much rocks go through the player, the game will stop

--- Assets/Script/DestroyOutOfBound.cs
+++ Assets/Script/DestroyOutOfBound.cs
@@ -2,23 +2,35 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
+   public MissingRockCounting missingRockCounting;
      private float topBound = 30;
      private float lowerBound = -10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        missingRockCounting = FindObjectOfType<MissingRockCounting>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound)
+      if (transform.position.z > topBound)
+      {
+         Destroy(gameObject);
+
+      }
+      else if (transform.position.z < lowerBound)
+      {
+         if (CompareTag("Obstacle"))
          {
-            Destroy(gameObject);
-            
-         } else if (transform.position.z < lowerBound){
-            Destroy(gameObject);
+            missingRockCounting.UpdateMissingText(1);
+            if (missingRockCounting.amount == 10)
+            {
+               Time.timeScale = 0.0f;
+            }
          }
+         Destroy(gameObject);
+         
+      }
     }
 }
--- Assets/Script/MissingRockCounting.cs
+++ Assets/Script/MissingRockCounting.cs
@@ -4,7 +4,7 @@ using TMPro;
 public class MissingRockCounting : MonoBehaviour
 {
     public TextMeshProUGUI missingText;
-    private int amount;
+    public int amount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
