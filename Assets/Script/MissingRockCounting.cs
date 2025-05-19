6bcf20782ff524a4eed664dcfca29552345b512d
Ly_Man <lym861@hsd.k12.or.us>
2025-05-19 12:04:38 -0700

    adding missing rock text

---
+++ Assets/Script/MissingRockCounting.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class MissingRockCounting : MonoBehaviour
+{
+    public TextMeshProUGUI missingText;
+    private int amount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateMissingText(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void UpdateMissingText(int amounttoAdd)
+    {
+        amount += amounttoAdd;
+        missingText.text = "Missing: " + amount + " /10";
+    }
+}
