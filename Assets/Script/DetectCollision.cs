17e39e8a45589c1c8a58f2a939935326d095e818
Ly_Man <lym861@hsd.k12.or.us>
2025-05-21 11:47:12 -0700

    adding sound effects when rocks and animals get hit by small rock

--- Assets/Script/DetectCollision.cs
+++ Assets/Script/DetectCollision.cs
@@ -5,12 +5,17 @@ public class DetectCollision : MonoBehaviour
     public ScoreManager scoreManager;
     public LiveManager liveManager;
     public int pointValue;
-    public ParticleSystem explosionParticle; 
+    private AudioSource objectAudio;
+    public AudioClip rockHitSound;
+    public AudioClip animalHitSound;
+
+    public ParticleSystem explosionParticle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
         liveManager = FindObjectOfType<LiveManager>();
+        objectAudio = GetComponent<AudioSource>();
     }
     // Update is called once per frame
     void Update()
@@ -23,14 +28,16 @@ public class DetectCollision : MonoBehaviour
         {
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             scoreManager.UpdateScore(pointValue);
+            objectAudio.PlayOneShot(rockHitSound, 1.0f);
+            AudioSource.PlayClipAtPoint(rockHitSound, Camera.main.transform.position);
         }
         else if (CompareTag("Animal"))
         {
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             liveManager.UpdateLiveText(-1);
-        }
-        Destroy(gameObject);
-        
+            AudioSource.PlayClipAtPoint(animalHitSound, Camera.main.transform.position);
+        }   
+        Destroy(gameObject);    
     }   
     
 }
